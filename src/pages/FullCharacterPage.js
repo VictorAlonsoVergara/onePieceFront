60e37f51363f7abdaf820c076bb044e70fe6ce3d
VictorAlonsoVergara <victor.vergarah@pucp.pe>
2023-03-24 15:19:58 -0500

    Pruebas de FullCharacter Page

--- src/pages/FullCharacterPage.js
+++ src/pages/FullCharacterPage.js
@@ -1,12 +1,24 @@
-import { useContext } from "react";
-import { useNavigate } from "react-router-dom"
+import { useContext, useEffect, useState } from "react"
 import { Context } from "../context/OnePieceContext";
+import MultiActionAreaCard from "../component/MultiActionAreaCard"
+import { useParams } from "react-router-dom";
 
 export function FullCharacterPage({id}){
     const {getFullCharacters} = useContext(Context);
-    console.log(getFullCharacters(id));
-
+    const [fullCharacters, setFullCharacters] = useState([]);
+    const params = useParams();
+    useEffect( () => {
+        (async () => {
+            const data = await getFullCharacters(params.id);
+            setFullCharacters(data);
+        })()
+    }, [params.id, getFullCharacters])
+    console.log(fullCharacters);
     return (
-        <div> Hola mundo </div>
+        <div>             
+            {fullCharacters.map(data => (
+                <MultiActionAreaCard data = {data} url = "/fullcharacters/" key = {data.idCharacter}/>
+            ))}
+        </div>
     );
 }
